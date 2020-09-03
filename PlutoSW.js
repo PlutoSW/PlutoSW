a081eb903cb84872326735ce40c1c0374b41813f
PlutoSW <iletisim@yasinersoy.com.tr>
2020-09-03 10:45:01 +0300

    implement elements

--- PlutoSW.js
+++ PlutoSW.js
@@ -90,6 +90,7 @@ class PlutoComponent {
         return this.render();
     }
 }
+window.PlutoImplements = [];
 window.PlutoSupportedTags = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"];
 const Pluto = {
     query: (query) => {
@@ -123,7 +124,7 @@ const Pluto = {
         return Pluto[name];
     },
     implement: (name, props) => {
-        return Pluto[name].props(props);
+        PlutoImplements[name] = props;
     }
 }
 JSON.highlight = Pluto.jsonHighlight;
@@ -132,6 +133,10 @@ JSON.pretty = Pluto.jsonPretty;
 window.PlutoSupportedTags.forEach(e => {
     Object.defineProperty(Pluto, e, {
         get() {
+            if (PlutoImplements[e]) {
+                var element = new PlutoElement(e);
+                return element.props(PlutoImplements[e]);
+            }
             return new PlutoElement(e);
         }
     });
