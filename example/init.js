acdfddcdc19ad6e97b4b378df1c77ddd888c9a2d
ERSOY <31663648+PlutoSW@users.noreply.github.com>
2023-04-22 23:49:07 +0300

    Update init.js

--- example/init.js
+++ example/init.js
@@ -1,30 +1,12 @@
 import {
-    Pluto,
-    ul
-} from './ul.js';
-window.storeddata = (localStorage.data) ? JSON.parse(localStorage.data) : null;
-var result = Pluto.pre,
-    ulcontainer = {
-        component: ul,
-        props: {
-            name: "ul",
-            result: result,
-            data: storeddata || [{
-                "key": "li 1 (Click for edit)"
-            }, {
-                "key": "li 2 (Click for edit)"
-            }]
-        }
-    };
-Pluto.query(document.getElementById("root")).render(
-    ulcontainer,
-    Pluto.button.text("ekle").on("click", () => {
-        PlutoComponents.ul.pushData({
-            "key": "li " + (PlutoComponents.ul.data.length + 1)
-        });
-    }),
-    Pluto.button.text("getir").on("click", () => {
-        result.html(JSON.highlight(PlutoComponents.ul.data));
-    }),
-    result
-)
+    Pluto
+} from '../PlutoSW.js';
+import Tab from 'tab.js';
+
+var tab = new tab({
+                name: "ProfileTab",
+                tabs: ["account", "cards", "addresses", "invoices", "logout"],
+                content: [Pluto.span("profile"), Pluto.span("cards") , "addresses", "invoices", "logout"],
+            });
+
+tab.go(2)
